eb9065e35c4f2e7e88447abd529be2d7e04cfaa4
robinhood14 <35225066+robinhood14@users.noreply.github.com>
2018-01-17 20:50:03 +0000

    Bot twitter fonction like recent tweet

--- twitterbot.rb
+++ twitterbot.rb
@@ -3,22 +3,22 @@ require 'twitter'
 require 'pry'
 
 client = Twitter::Streaming::Client.new do |config|
-  config.consumer_key        = ""
-  config.consumer_secret     = ""
-  config.access_token        = ""
-  config.access_token_secret = ""
+  config.consumer_key        = "XXXXX"
+  config.consumer_secret     = "XXXXX"
+  config.access_token        = "XXXXX"
+  config.access_token_secret = "XXXXX"
 end
 
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ""
-  config.consumer_secret     = ""
-  config.access_token        = ""
-  config.access_token_secret = ""
+  config.consumer_key        = "XXXXX"
+  config.consumer_secret     = "XXXXX"
+  config.access_token        = "XXXXX"
+  config.access_token_secret = "XXXXX"
 end
 
 
-# Un bot qui like tous les tweets faits par une certaine liste de journalistes, dès qu'ils sortent. (like les tweets récents (20 derniers) grace a la méthode tweet_id )
+# Création d'un bot qui like tous les tweets faits par une certaine liste de journalistes, dès qu'ils sortent. (like les tweets récents (20 derniers) grace a la méthode tweet_id )
 
 #Creer une liste avec les identifiant des utilisateurs.
 list_journalist = ["@jeanmidata1","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon"]
@@ -28,4 +28,4 @@ list_journalist.each do |x|  #on itere avec la méthode each do |x| pour appliqu
     client.favorite(tweet_id) #on applique la methode client.favorite qui Like la liste des tweets
 end
 
-
+ 
